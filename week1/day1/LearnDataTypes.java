b2c5acec755316df6a77cdc39710a9e3f54fda95
Dhanalakshmi7708 <dhanagomathi.29@gmail.com>
2025-03-30 08:04:14 +0530

    Add files via upload
    Week 1 day1 files uploaded

---
+++ week1/day1/LearnDataTypes.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class LearnDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	byte age=30;
+	System.out.println(age);
+	
+	short depositAmount = 30000;
+	System.out.println(depositAmount);
+	
+	int number = 80000;
+	
+	
+
+	}
+
+}
