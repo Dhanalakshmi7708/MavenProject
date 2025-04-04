a40bc45826258e741cfac2b34e533b12233b9556
Dhanalakshmi7708 <dhanagomathi.29@gmail.com>
2025-04-04 14:02:25 +0530

    Update PalindromeCheck.java
    updated code

--- week1/day2/PalindromeCheck.java
+++ week1/day2/PalindromeCheck.java
@@ -5,7 +5,31 @@ public class PalindromeCheck {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int num = 3353 ;
+		int num = 3553, revnum = 0,remainder;
+		//store num in Orignum
+		
+		int Orignum =num;
+		
+		//Using while loop to reverse the number 
+		
+		while (num != 0) {
+			 remainder = num % 10;
+		      revnum = revnum * 10 + remainder;
+		      num /= 10;
+		}
+		
+		//if condition to check given number is Palindrome number or not 
+		
+		if (Orignum == revnum) {
+			
+			System.out.println(Orignum + " is Palindrome Number");
+		}
+		
+		else
+		{
+			System.out.println(Orignum + " is not Palindrome Number");
+		}
+		
 	}
 
 }
