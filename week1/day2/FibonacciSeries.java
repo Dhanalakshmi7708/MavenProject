51d3d4ca0455bc59bcc433854cc249c5624f3d0f
Dhanalakshmi7708 <dhanagomathi.29@gmail.com>
2025-04-04 14:01:17 +0530

    Update FibonacciSeries.java
    Updated Code for FinonacciSeries

--- week1/day2/FibonacciSeries.java
+++ week1/day2/FibonacciSeries.java
@@ -4,7 +4,19 @@ public class FibonacciSeries {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
+	    int n = 8, N1 = 0, N2 = 1;
+	    System.out.println("Fibonacci Series of " + n + " terms:");
+
+	    for (int i = 1; i <= n; ++i) {
+	      System.out.print(N1 + ", ");
+
+	      // compute the next term
+	      int series = N1 + N2;
+	      N1 = N2;
+	      N2 = series;
+	    }
 	}
 
 }
