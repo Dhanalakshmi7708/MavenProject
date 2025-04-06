1bdfd8a47c9e0a387bc9a9592a6d757c2b285b25
Dhanalakshmi7708 <dhanagomathi.29@gmail.com>
2025-04-06 06:34:08 +0530

    Update Library.java

--- week2/Library.java
+++ week2/Library.java
@@ -2,20 +2,21 @@ package week2.day1;
 
 public class Library {
 
-	public String addBook (String booktitle) {
-		System.out.println(booktitle+ " book added Successfully");
-		return booktitle;
+	public String addBook (String bookTitle) //define method1 
+	{
+		System.out.println(bookTitle+ " book added Successfully");
+		return bookTitle;
 	}
 	
-	public void issueBook(String bookname) {
-		System.out.println(bookname +" "+"Book issued successfully");
+	public void issueBook(String bookName)//define method2
+	{
+		System.out.println(bookName +" "+"Book issued successfully");
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Library bookDetail = new Library();
-		bookDetail.addBook("HarryPotter");
-		bookDetail.issueBook("Ponniyin Selvan");
+			
+		Library bookDetail = new Library(); //declaring method in main method using object 
+		bookDetail.addBook("HarryPotter"); //call the method bookTitle
+		bookDetail.issueBook("Ponniyin Selvan");//call the method bookName
 		
 
 	}
