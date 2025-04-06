7aaac57b35586022238ddfb8e8c7fe08b85bb8cb
Dhanalakshmi7708 <dhanagomathi.29@gmail.com>
2025-04-06 06:34:43 +0530

    Update LibraryManagement.java

--- week2/LibraryManagement.java
+++ week2/LibraryManagement.java
@@ -1,11 +1,12 @@
 package week2.day1;
 
-public class LibraryManagement {
+public class LibraryManagement //create the class - LibraryManagement
+{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Library bookDetail2 = new Library();
+		Library bookDetail2 = new Library(); //create object to call the method from the other call 
 		bookDetail2.addBook("Harry Potter and the Philosopher's Stone");
 		bookDetail2.issueBook("Ponniyin Selvan");
 	}
